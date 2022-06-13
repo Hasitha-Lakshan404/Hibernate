ad55ff63f5cda7af4832662891a51cdec4500a81
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-14 03:27:47 +0530

    Added Hibernate.cfg.Xml /Libraries

---
+++ src/AppInitializer.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Hasitha Lakshan
+ * Project :SuperMarket_WIth_Hibernate
+ * Date :6/14/2022
+ * Time :3:20 AM
+ */
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
